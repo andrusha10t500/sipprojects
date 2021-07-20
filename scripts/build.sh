35942106264b4842e1292b2a6f8ec77039d1bf07
andrusha10t500 <andrusha10t500@gmail.com>
2021-07-20 22:00:13 +1000

    correct scripts

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-$containers=$(docker ps -qa)
-$images=$(docker images -qa)
+containers=$(docker ps -qa)
+images=$(docker images -qa)
 
 docker stop $containers
 
@@ -13,11 +13,11 @@ if [[ -n "$images" ]]
       docker rmi $images
 fi
 
-docker-compose up -d
-
-docker-compose exec -T backend php artisan key:generate
-docker-compose exec -T backend php artisan migrate
-docker-compose exec -T backend php artisan storage:link
-docker-compose exec -T backend composer install
-docker-compose exec -T backend npm install
-docker-compose exec -T backend npm run dev
+#docker-compose up -d
+#
+#docker-compose exec -T backend php artisan key:generate
+#docker-compose exec -T backend php artisan migrate
+#docker-compose exec -T backend php artisan storage:link
+#docker-compose exec -T backend composer install
+#docker-compose exec -T backend npm install
+#docker-compose exec -T backend npm run dev
